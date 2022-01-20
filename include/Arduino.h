21278b1f7038570d695f21bca399d172840936ac
Jan Hermes <jan@hermes-technology.de>
2022-01-20 09:23:25 +0100

    fix: add radians and uint8_t

--- include/Arduino.h
+++ include/Arduino.h
@@ -2,8 +2,13 @@
 
 #include <string>
 #include <cstdint>
+#include <cmath>
 
 #include "Wire.h"
 #include "api/Common.h"
+
+float radians(float degrees) {
+    return degrees * (M_PI / 180.0);
+}
 
 using String = std::string;
--- include/api/Common.h
+++ include/api/Common.h
@@ -1,5 +1,18 @@
 #pragma once
 
+#include <cstdint>
+#include <chrono>
+
+unsigned long millis() {
+    using namespace std::chrono;
+
+    auto epoch = high_resolution_clock::from_time_t(0);
+    // ...
+    auto now   = high_resolution_clock::now();
+
+    return duration_cast<milliseconds>(now - epoch).count();
+}
+
 enum PinStatus {
     LOW = 0,
     HIGH = 1
@@ -11,3 +24,5 @@ enum PinMode {
     OUTPUT = 3
 };
 using pin_size_t = uint8_t;
+
+
